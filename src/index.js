70b2350008e937d8804d85df3d8c0c00ba2b6f00
Bogdan Dyachenko <46127718+Bodyan-d@users.noreply.github.com>
2021-06-05 16:40:20 +0300

    add-murkup

--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
 import './sass/main.scss';
 import fetchImages from './apiService';
+import cardImagesTpl from './templates/cardImage.hbs'
+
+const galleryRef = document.querySelector('.gallery-js');
+const searchRef = document.querySelector('.search-form-input');
+
+searchRef.addEventListener('input', onInput);
+
+function onInput(e) {
+
+    const images = e.target.value;
+    if (!images.trim()) {
+        return;
+    }
+
+    fetchImages(images).then(r => {
+        galleryRef.innerHTML = "";
+        addMurkup(r);
+    });
+}
+
+function addMurkup(images) {
+    const imagesMurkup = cardImagesTpl(images);
+
+    galleryRef.insertAdjacentHTML('beforeend', imagesMurkup);
+};
